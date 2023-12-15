399d5ee7248037b631b9fc283c1070f8975b19d2
Kaíque Freire <kaiquefreiresantos05@gmail.com>
2023-12-15 09:16:52 -0300

    added first data to database

--- sqlAlchemyApplication.py
+++ sqlAlchemyApplication.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import Session
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column
 from sqlalchemy import inspect
@@ -42,7 +43,7 @@ class Address(Base):
     user = relationship("User", back_populates="address")
 
     def __repr__(self):  # Construtor
-        return f"Address (id={self.id}, email={self.email_address})"  # Não precisa inserir a chave estrangeira
+        return f"Address (id={self.id}, email_address={self.email_address})"  # Não precisa inserir a chave estrangeira
 
 
 # Conexão com o DataBase
@@ -59,3 +60,27 @@ inspetor_engine = inspect(engine)
 print(inspetor_engine.has_table("user_account"))  # Retornando True
 print(inspetor_engine.get_table_names())  # Vendo os nomes das tabelas criadas
 print(inspetor_engine.default_schema_name)
+
+# With, inicia uma sessão com o banco de dados especificado pelo engine, ela garante que a sessão é fechada corretamente
+with Session(engine) as session:
+    kaique = User(  # Variável do tipo User e também vai ter a estrutura de address
+        name='Kaique',
+        fullname='Kaique Freire',
+        address=[Address(email_address='kaiquefreire@gmail.com')]
+    )
+
+    maria = User(
+        name='Maria',
+        fullname='Maria Teresa',
+        address=[Address(email_address='maria@gamail.com'),  # Forma de adicionar 2 emails diferentes, DB RELACIONAL!!!
+                 Address(email_address='mariasecundario@gmail.org')]
+    )
+
+    victor = User(
+        name='Victor', fullname='Victor Peixoto'
+    )
+
+    # Enviado para o DataBase (persistência de dados)
+    session.add_all([kaique, maria, victor])
+
+    session.commit()  # Marca o fim da transação e envia para o DB
